7385fbf8d778e9c9dd1bd23e4fe23280ca510c04
Your Name <you@example.com>
2024-01-26 20:04:34 -0500

    adding basic econding and compression, thinking of moving towards linked list map combination, or id in sparse trie for range and encode time for example

--- model.go
+++ model.go
@@ -2,12 +2,38 @@ package webkit
 
 import database "github.com/multiverse-os/webkit/database"
 
+const (
+	Uncompressed CompressionType = iota
+	LZ
+	ZStd
+	Snappy
+)
+
+// TODO: Probably best to just find a binary to struct then modify it to support
+//
+//	our relevant types
+const (
+	Unencoded EncodingType = iota
+	JSON
+	CBOR
+	BSON
+)
+
+// TODO: Then maybe we save serialize into BSON or CBOR to create our own object
+//
+//	store
 type Model struct {
 	Framework *Framework
 
 	Database *database.Database
 
 	Name string
+
+	Key   []byte
+	Value []byte
+
+	Compression CompressionType
+	Encoding    EncodingType
 
 	BeforeCreate []func()
 	AfterCreate  []func()
